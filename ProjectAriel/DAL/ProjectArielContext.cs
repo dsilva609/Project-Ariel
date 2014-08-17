6693b1c9e0b64e29f7a2d67d1feba8d0ba160649
dsilva609 <dsilva609@gmail.com>
2014-08-16 20:59:44 -0400

    initial commit

---
+++ ProjectAriel/DAL/ProjectArielContext.cs
@@ -0,0 +1,26 @@
+﻿using ProjectAriel.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using System.Linq;
+using System.Web;
+
+namespace ProjectAriel.DAL
+{
+	public class ProjectArielContext : DbContext
+	{
+		public DbSet<Player> Players { get; set; }
+
+		public ProjectArielContext()
+			: base("ProjectAriel")
+		{
+
+		}
+
+		protected override void OnModelCreating(DbModelBuilder modelBuilder)
+		{
+			modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+		}
+	}
+}
---
+++ ProjectAriel/Models/Player.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ProjectAriel.Models
+{
+	public class Player
+	{
+		public string Name { get; set; }
+		public int ID { get; set; }
+		public bool IsActive { get; set; }
+	}
+}
