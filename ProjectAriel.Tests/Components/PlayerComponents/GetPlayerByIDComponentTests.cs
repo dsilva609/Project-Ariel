a0ad717fc392fc2b60b36cd7e9dc18e320024e4a
dsilva609 <dsilva609@gmail.com>
2014-08-21 14:18:35 -0400

    added test for get player by id component

---
+++ ProjectAriel.Tests/Components/PlayerComponents/GetPlayerByIDComponentTests.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using ProjectAriel.Repositories;
+using ProjectAriel.Models;
+using ProjectAriel.Components.PlayerComponents;
+using Moq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ProjectAriel.Tests.Components.PlayerComponents
+{
+	[TestClass]
+	public class GetPlayerByIDComponentTests
+	{
+		private IRepository<Player> _repo;
+		private GetPlayerByIDComponent _getPlayerByIDComponent;
+		private Mock<IRepository<Player>> _mock;
+
+		[TestInitialize]
+		public void Setup()
+		{
+			this._mock = new Mock<IRepository<Player>>();
+			this._getPlayerByIDComponent = new GetPlayerByIDComponent();
+		}
+
+		[TestMethod]
+		public void TestThatPlayerOfMatchingIDIsReturned()
+		{
+			//--Arrange	
+			this._mock.Setup(m => m.GetByID(1)).Returns(new Player { ID = 1, Name = "Smitty Werbenjagermanjensen", IsActive = true });
+			this._repo = this._mock.Object;
+
+			//--Act
+			var result = this._getPlayerByIDComponent.Execute(this._repo, 1);
+
+			//--Assert
+			Assert.AreEqual(result.ID, 1);
+		}
+	}
+}
