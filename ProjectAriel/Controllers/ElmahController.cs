6c552aaa77f457d8c448a03f6eea960151fa3c00
Dennis D. Silva <dsilva609@gmail.com>
2014-09-05 16:51:40 -0400

    secured elmah for admins only

--- ProjectAriel/Controllers/ElmahController.cs
+++ ProjectAriel/Controllers/ElmahController.cs
@@ -1,12 +1,9 @@
 ﻿using ProjectAriel.Common;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
 using System.Web.Mvc;
 
 namespace ProjectAriel.Controllers
 {
+	[Authorize(Roles = "Admin")]
 	public partial class ElmahController : Controller
 	{
 		public virtual ActionResult Index(string type)
