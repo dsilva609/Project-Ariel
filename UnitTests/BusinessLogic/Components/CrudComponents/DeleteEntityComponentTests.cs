5334d07b19ef01c5eb699967fb09ac02973bdaa9
dsilva609 <dsilva609@gmail.com>
2015-02-05 11:36:03 -0500

    refractored delete entity components tests

--- UnitTests/BusinessLogic/Components/CrudComponents/DeleteEntityComponentTests.cs
+++ UnitTests/BusinessLogic/Components/CrudComponents/DeleteEntityComponentTests.cs
@@ -1,29 +1,19 @@
-﻿using BusinessLogic.Components.CrudComponents;
-using BusinessLogic.Enums;
+﻿using BusinessLogic.Enums;
 using BusinessLogic.Models;
-using BusinessLogic.Repositories;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
-using Moq;
 
 namespace UnitTests.BusinessLogic.Components.CrudComponents
 {
 	[TestClass]
-	public class DeleteEntityComponentTests
+	public class DeleteEntityComponentTests : DeleteEntityComponentTestBase
 	{
-		private IRepository<Card> _cardRepo;
-		private IRepository<Player> _playerRepo;
-		private DeleteEntityComponent _deleteEntityComponent;
-		private Mock<IRepository<Card>> _cardRepositoryMock;
-		private Mock<IRepository<Player>> _playerRepositoryMock;
-		private Card _card;
-		private Player _player;
+		protected Card _card;
+		protected Player _player;
 
 		[TestInitialize]
-		public void Setup()
+		public override void Setup()
 		{
-			this._deleteEntityComponent = new DeleteEntityComponent();
-			this._cardRepositoryMock = new Mock<IRepository<Card>>();
-			this._playerRepositoryMock = new Mock<IRepository<Player>>();
+			base.Setup();
 
 			this._card = new Card
 			{
@@ -45,14 +35,14 @@ namespace UnitTests.BusinessLogic.Components.CrudComponents
 		}
 
 		[TestMethod]
-		public void TestThatCardIsRemovedFromTheRepository()
+		public void ThatCardIsRemovedFromTheRepository()
 		{
 			//--Arrange
-			this._cardRepositoryMock.Setup(m => m.Add(this._card));
-			this._cardRepo = this._cardRepositoryMock.Object;
+			base._cardRepositoryMock.Setup(m => m.Add(this._card));
+			base._cardRepo = base._cardRepositoryMock.Object;
 
 			//--Act
-			this._deleteEntityComponent.Execute(this._cardRepo, this._card.ID);
+			base._deleteEntityComponent.Execute(base._cardRepo, this._card.ID);
 			var result = this._cardRepo.GetByID(this._card.ID);
 
 			//--Assert
@@ -60,15 +50,15 @@ namespace UnitTests.BusinessLogic.Components.CrudComponents
 		}
 
 		[TestMethod]
-		public void TestThatPlayerIsRemovedFromTheRepository()
+		public void ThatPlayerIsRemovedFromTheRepository()
 		{
 			//--Arrange
-			this._playerRepositoryMock.Setup(m => m.Add(this._player));
-			this._playerRepo = this._playerRepositoryMock.Object;
+			base._playerRepositoryMock.Setup(m => m.Add(this._player));
+			base._playerRepo = base._playerRepositoryMock.Object;
 
 			//--Act
-			this._deleteEntityComponent.Execute(this._playerRepo, this._player.ID);
-			var result = this._playerRepo.GetByID(this._player.ID);
+			base._deleteEntityComponent.Execute(base._playerRepo, this._player.ID);
+			var result = base._playerRepo.GetByID(this._player.ID);
 
 			//--Assert
 			Assert.IsNull(result);
