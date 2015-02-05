e0897f8101ad6aa23627180f28dca20e7e9c9c9a
dsilva609 <dsilva609@gmail.com>
2015-02-05 12:10:15 -0500

    refactored service tests

--- UnitTests/BusinessLogic/Services/CardServiceTests.cs
+++ UnitTests/BusinessLogic/Services/CardServiceTests.cs
@@ -1,26 +1,18 @@
 ﻿using BusinessLogic.Models;
-using BusinessLogic.Repositories;
-using BusinessLogic.Services;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
-using Moq;
 using System.Collections.Generic;
 
 namespace UnitTests.BusinessLogic.Services
 {
 	[TestClass]
-	public class CardServiceTests
+	public class CardServiceTests : CardServiceTestBase
 	{
-		private Mock<IUnitOfWork> _Uow;
-		private Mock<CardService> _Service;
-		private Mock<IRepository<Card>> _cardRepositoryMock;
 		private List<Card> _cardList;
 
 		[TestInitialize]
-		public void Setup()
+		public override void Setup()
 		{
-			this._Uow = new Mock<IUnitOfWork>();
-			this._Service = new Mock<CardService>(this._Uow.Object);
-			this._cardRepositoryMock = new Mock<IRepository<Card>>();
+			base.Setup();
 
 			this._cardList = new List<Card>
 			{
@@ -48,28 +40,28 @@ namespace UnitTests.BusinessLogic.Services
 		}
 
 		[TestMethod]
-		public void TestThatGetAllMethodSortsAscending()
+		public void ThatGetAllMethodSortsAscending()
 		{
 			//--Arrange
-			this._Uow.Setup(x => x.GetRepository<Card>()).Returns(this._cardRepositoryMock.Object);
-			this._cardRepositoryMock.Setup(x => x.GetAll()).Returns(this._cardList);
+			base._Uow.Setup(x => x.GetRepository<Card>()).Returns(base._cardRepositoryMock.Object);
+			base._cardRepositoryMock.Setup(x => x.GetAll()).Returns(this._cardList);
 
 			//--Act
-			var results = this._Service.Object.GetAll(true, "Name");
+			var results = base._Service.Object.GetAll(true, "Name");
 
 			//--Assert
 			Assert.AreEqual("Arruga", results[0].Name);
 		}
 
 		[TestMethod]
-		public void TestThatGetAllMethodSortsDescending()
+		public void ThatGetAllMethodSortsDescending()
 		{
 			//--Arrange
-			this._Uow.Setup(x => x.GetRepository<Card>()).Returns(this._cardRepositoryMock.Object);
-			this._cardRepositoryMock.Setup(x => x.GetAll()).Returns(this._cardList);
+			base._Uow.Setup(x => x.GetRepository<Card>()).Returns(base._cardRepositoryMock.Object);
+			base._cardRepositoryMock.Setup(x => x.GetAll()).Returns(this._cardList);
 
 			//--Act
-			var results = this._Service.Object.GetAll(false, "Name");
+			var results = base._Service.Object.GetAll(false, "Name");
 
 			//--Assert
 			Assert.AreEqual("Thud", results[0].Name);
--- UnitTests/BusinessLogic/Services/PlayerServiceTests.cs
+++ UnitTests/BusinessLogic/Services/PlayerServiceTests.cs
@@ -1,26 +1,18 @@
 ﻿using BusinessLogic.Models;
-using BusinessLogic.Repositories;
-using BusinessLogic.Services;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
-using Moq;
 using System.Collections.Generic;
 
 namespace UnitTests.BusinessLogic.Services
 {
 	[TestClass]
-	public class PlayerServiceTests
+	public class PlayerServiceTests : PlayerServiceTestBase
 	{
-		private Mock<IUnitOfWork> _Uow;
-		private Mock<PlayerService> _Service;
-		private Mock<IRepository<Player>> _playerRepositoryMock;
 		private List<Player> _playerList;
 
 		[TestInitialize]
 		public void Setup()
 		{
-			this._Uow = new Mock<IUnitOfWork>();
-			this._Service = new Mock<PlayerService>(this._Uow.Object);
-			this._playerRepositoryMock = new Mock<IRepository<Player>>();
+			base.Setup();
 
 			this._playerList = new List<Player>
 			{
@@ -48,28 +40,28 @@ namespace UnitTests.BusinessLogic.Services
 		}
 
 		[TestMethod]
-		public void TestThatGetAllMethodOrdersAscending()
+		public void ThatGetAllMethodOrdersAscending()
 		{
 			//--Arrange
-			this._Uow.Setup(x => x.GetRepository<Player>()).Returns(this._playerRepositoryMock.Object);
-			this._playerRepositoryMock.Setup(x => x.GetAll()).Returns(this._playerList);
+			base._Uow.Setup(x => x.GetRepository<Player>()).Returns(base._playerRepositoryMock.Object);
+			base._playerRepositoryMock.Setup(x => x.GetAll()).Returns(this._playerList);
 
 			//--Act
-			var results = this._Service.Object.GetAll(true, "Name");
+			var results = base._Service.Object.GetAll(true, "Name");
 
 			//--Assert
 			Assert.AreEqual("Hank Moody", results[0].Name);
 		}
 
 		[TestMethod]
-		public void TestThatGetAllMethodOrdersDescending()
+		public void ThatGetAllMethodOrdersDescending()
 		{
 			//--Arrange
-			this._Uow.Setup(x => x.GetRepository<Player>()).Returns(this._playerRepositoryMock.Object);
-			this._playerRepositoryMock.Setup(x => x.GetAll()).Returns(this._playerList);
+			base._Uow.Setup(x => x.GetRepository<Player>()).Returns(base._playerRepositoryMock.Object);
+			base._playerRepositoryMock.Setup(x => x.GetAll()).Returns(this._playerList);
 
 			//--Act
-			var results = this._Service.Object.GetAll(false, "Name");
+			var results = base._Service.Object.GetAll(false, "Name");
 
 			//--Assert
 			Assert.AreEqual("Willis", results[0].Name);
