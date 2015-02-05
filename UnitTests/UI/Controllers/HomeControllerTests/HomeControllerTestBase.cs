cb9b01f7991519d50e7842d47b25311c25b1a92f
dsilva609 <dsilva609@gmail.com>
2015-02-05 10:05:13 -0500

    refactored home controller tests

---
+++ UnitTests/UI/Controllers/HomeControllerTests/HomeControllerTestBase.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+//using Moq;
+using NUnit.Framework;
+using UI.Controllers;
+
+
+namespace UnitTests.UI.Controllers.HomeControllerTests
+{
+	[TestClass]
+	public class HomeControllerTestBase
+	{
+		protected HomeController homeControllerMock;
+
+		[SetUp]
+		public virtual void Setup()
+		{
+			homeControllerMock = new HomeController();
+		}
+	}
+}
---
+++ UnitTests/UI/Controllers/HomeControllerTests/HomeControllerTests.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Web.Mvc;
+
+namespace UnitTests.UI.Controllers.HomeControllerTests
+{
+	[TestClass]
+	public class HomeControllerTests : HomeControllerTestBase
+	{
+		[TestMethod]
+		public void Index()
+		{
+			//--Arrange
+			base.Setup();
+
+			//--Act
+			var result = base.homeControllerMock.Index() as ViewResult;
+
+			// Assert
+			Assert.IsNotNull(result);
+		}
+
+		[TestMethod]
+		public void About()
+		{
+			//--Arrange
+			base.Setup();
+
+			//--Act
+			var result = base.homeControllerMock.About() as ViewResult;
+
+			//--Assert
+			Assert.AreEqual("Your application description page.", result.ViewBag.Message);
+		}
+
+		[TestMethod]
+		public void Contact()
+		{
+			// Arrange
+			base.Setup();
+
+			//--Act
+			var result = base.homeControllerMock.Contact() as ViewResult;
+
+			//--Assert
+			Assert.IsNotNull(result);
+		}
+	}
+}
