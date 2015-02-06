029efd61625e72c2daa3f1fab2e9dedd397ec042
dsilva609 <dsilva609@gmail.com>
2015-02-06 13:12:14 -0500

    started player controller tests

---
+++ UnitTests/UI/Controllers/PlayerControllerTests/PlayerControllerTestBase.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTests.UI.Controllers.PlayerControllerTests
+{
+	[TestClass]
+	public class PlayerControllerTestBase
+	{
+		[TestInitialize]
+		public virtual void Setup()
+		{
+		}
+	}
+}
---
+++ UnitTests/UI/Controllers/PlayerControllerTests/PlayerControllerTests.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTests.UI.Controllers.PlayerControllerTests
+{
+	[TestClass]
+	public class PlayerControllerTests : PlayerControllerTestBase
+	{
+	}
+}
