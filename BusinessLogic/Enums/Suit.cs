d92b482d5f92d07553f623b70d39c95e956baebd
dsilva609 <dsilva609@gmail.com>
2015-02-06 12:10:10 -0500

    added suit enum tests

--- BusinessLogic/Enums/Suit.cs
+++ BusinessLogic/Enums/Suit.cs
@@ -5,11 +5,11 @@ namespace BusinessLogic.Enums
 	public enum Suit
 	{
 		[Display(Name = "-Select Suit-")]
-		Default,
+		Default,							//0
 
-		Heart,
-		Diamond,
-		Club,
-		Spade
+		Heart,								//1
+		Diamond,							//2
+		Club,								//3
+		Spade								//4
 	}
 }
