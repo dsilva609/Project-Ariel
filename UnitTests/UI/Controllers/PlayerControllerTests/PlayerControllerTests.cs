20bed46b8a1b4fb8f3a92350f70d7003173692ce
dsilva609 <dsilva609@gmail.com>
2015-02-06 13:19:00 -0500

    setup some initializers for test base

--- UnitTests/UI/Controllers/PlayerControllerTests/PlayerControllerTests.cs
+++ UnitTests/UI/Controllers/PlayerControllerTests/PlayerControllerTests.cs
@@ -1,9 +1,18 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using UI.Controllers;
 
 namespace UnitTests.UI.Controllers.PlayerControllerTests
 {
 	[TestClass]
 	public class PlayerControllerTests : PlayerControllerTestBase
 	{
+		protected Mock<PlayerController> _playerController;
+
+		[TestInitialize]
+		public virtual void Setup()
+		{
+			this._playerController = new Mock<PlayerController>();
+		}
 	}
 }
