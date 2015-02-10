1629558b6cc642e8f12166320b01cd71be4b476c
dsilva609 <dsilva609@gmail.com>
2015-02-10 17:10:58 -0500

    added sort player test

---
+++ UnitTests/UI/Controllers/PlayerControllerTests/SortPlayerTests.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using System.Web.Mvc;
+
+namespace UnitTests.UI.Controllers.PlayerControllerTests
+{
+	[TestClass]
+	public class SortPlayerTests : PlayerControllerTestBase
+	{
+		[TestMethod]
+		public void ThatSortPlayerActionReturnsAView()
+		{
+			//--Arrange
+			base._playerController.Setup(mock => mock.SortPlayers(It.IsNotNull<string>())).Returns(new ViewResult { ViewName = MVC.Player.Views.Index });
+
+			//--Act
+			var result = base._playerController.Object.SortPlayers("Newman") as ViewResult;
+
+			//--Assert
+			Assert.AreEqual(MVC.Player.Views.Index, result.ViewName);
+		}
+	}
+}
