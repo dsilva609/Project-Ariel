bd147788a30d891126f4183fe916cef3b478840b
dsilva609 <dsilva609@gmail.com>
2015-02-23 12:55:03 -0500

    added card controller setup class

---
+++ UnitTests/UI/Controllers/CardControllerTests/CardControllerTestBase.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using UI.Controllers;
+
+namespace UnitTests.UI.Controllers.CardControllerTests
+{
+	[TestClass]
+	public class CardControllerTestBase
+	{
+		protected Mock<CardController> _cardController;
+
+		[TestInitialize]
+		public virtual void Setup()
+		{
+			this._cardController = new Mock<CardController>();
+		}
+	}
+}
