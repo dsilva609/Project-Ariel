db2ea1e0fd45467abe587be1d6ab0dd24855c79f
dsilva609 <dsilva609@gmail.com>
2015-02-23 14:01:19 -0500

    started sort card tests

---
+++ UnitTests/UI/Controllers/CardControllerTests/SortCardTests.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using System.Web.Mvc;
+
+namespace UnitTests.UI.Controllers.CardControllerTests
+{
+	[TestClass]
+	public class SortCardTests : CardControllerTestBase
+	{
+		[TestInitialize]
+		public override void Setup()
+		{
+			base.Setup();
+		}
+
+		[TestMethod]
+		public void ThatSortPlayerActionReturnsAView()
+		{
+			//--Arrange
+			base._cardController.Setup(mock => mock.SortPlayers(It.IsNotNull<string>())).Returns(new ViewResult { ViewName = MVC.Card.Views.Index });
+
+			//--Act
+			var result = base._cardController.Object.SortPlayers("apple") as ViewResult;
+
+			//--Assert
+			Assert.AreEqual(MVC.Card.Views.Index, result.ViewName);
+		}
+	}
+}
